aa50f01034655892e6402d168ad6373b1b35a493
Leon Rasberry <lras1@hotmail.com>
2026-03-23 19:52:39 -0400

    Fix DI registration for device library dependencies

--- CalibrationTuning/Program.cs
+++ CalibrationTuning/Program.cs
@@ -3,7 +3,15 @@ using System.Windows.Forms;
 using Microsoft.Extensions.DependencyInjection;
 using CalibrationTuning.Controllers;
 using Tegam._1830A.DeviceLibrary.Services;
+using Tegam._1830A.DeviceLibrary.Communication;
+using Tegam._1830A.DeviceLibrary.Commands;
+using Tegam._1830A.DeviceLibrary.Parsing;
+using Tegam._1830A.DeviceLibrary.Validation;
 using Siglent.SDG6052X.DeviceLibrary.Services;
+using SiglentComm = Siglent.SDG6052X.DeviceLibrary.Communication;
+using SiglentCmd = Siglent.SDG6052X.DeviceLibrary.Commands;
+using SiglentParse = Siglent.SDG6052X.DeviceLibrary.Parsing;
+using SiglentVal = Siglent.SDG6052X.DeviceLibrary.Validation;
 
 namespace CalibrationTuning
 {
@@ -35,6 +43,20 @@ namespace CalibrationTuning
         /// </summary>
         private static void ConfigureServices(IServiceCollection services)
         {
+            // Register Tegam 1830A device library dependencies
+            services.AddSingleton<Tegam._1830A.DeviceLibrary.Communication.IVisaCommunicationManager, 
+                Tegam._1830A.DeviceLibrary.Communication.VisaCommunicationManager>();
+            services.AddSingleton<IScpiCommandBuilder, ScpiCommandBuilder>();
+            services.AddSingleton<IScpiResponseParser, ScpiResponseParser>();
+            services.AddSingleton<IInputValidator, InputValidator>();
+            
+            // Register Siglent SDG6052X device library dependencies
+            services.AddSingleton<SiglentComm.IVisaCommunicationManager, 
+                SiglentComm.VisaCommunicationManager>();
+            services.AddSingleton<SiglentCmd.IScpiCommandBuilder, SiglentCmd.ScpiCommandBuilder>();
+            services.AddSingleton<SiglentParse.IScpiResponseParser, SiglentParse.ScpiResponseParser>();
+            services.AddSingleton<SiglentVal.IInputValidator, SiglentVal.InputValidator>();
+            
             // Register device services
             services.AddSingleton<IPowerMeterService, PowerMeterService>();
             services.AddSingleton<ISignalGeneratorService, SignalGeneratorService>();
